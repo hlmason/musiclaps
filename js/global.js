7d6603366758e365d5cbe79df78b6e8e28a1c29a
Heidi Mason <heidi@musiclaps.com>
2014-07-03 15:50:42 -0700

    Polished nav, got footer working, made h1 responsive

--- js/global.js
+++ js/global.js
@@ -1,13 +1,10 @@
 var navbar_right = $('.navbar-right');
-// var h1_landing_page_photo = $('#landing-page-photo h1');
 
 $(window).resize(function() {
   if ($(window).width() <= 991) { /* Matches media query max-width on main.css */
     navbar_right.addClass('pull-left');
-    // h1_landing_page_photo.addClass('pull-left');
   }
   else {
   	navbar_right.removeClass('pull-left');
-    // h1_landing_page_photo.removeClass('pull-left');
   }
 });
