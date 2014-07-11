58cf3654b1b78e0a5d3d8f4c39dd01a4d69ea85c
Heidi Mason <heidi@musiclaps.com>
2014-07-10 17:46:50 -0700

    Refactor

--- js/global.js
+++ js/global.js
@@ -15,13 +15,9 @@ var sign_in_out = $('.sign-in-out-js');
 
 // Makes record buttons larger upon hover
 $(sign_in_out).mouseenter(function() {
-	if ($(this).width('90%')) {
   	$(this).css('width', '100%').css('margin-top', -20);
-  }
 });
 
 $(sign_in_out).mouseleave(function() {
-	if ($(this).width('100%')) {
 		$(this).css('width', '90%').css('margin-top', -15);
-	}
 });
