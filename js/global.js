c76eb394c5022e51f951e5eafeafd24799177b43
Heidi Mason <heidi@musiclaps.com>
2014-07-10 17:22:34 -0700

    Make sign in button interactive, add HTML5 Shim

--- js/global.js
+++ js/global.js
@@ -1,5 +1,6 @@
 var navbar_right = $('.navbar-right');
 
+// Pulls navbar left in the period before Bootstrap automatically collapses it
 $(window).resize(function() {
   if ($(window).width() <= 991) { /* Matches media query max-width on main.css */
     navbar_right.addClass('pull-left');
@@ -7,4 +8,20 @@ $(window).resize(function() {
   else {
   	navbar_right.removeClass('pull-left');
   }
+});
+
+
+var sign_in = $('#sign-in-js');
+
+// Makes record buttons larger upon hover
+$(sign_in).mouseenter(function() {
+	if (sign_in.width('90%')) {
+  	$(this).css('width', '100%').css('margin-top', -20);
+  }
+});
+
+$(sign_in).mouseleave(function() {
+	if (sign_in.width('100%')) {
+		$(this).css('width', '90%').css('margin-top', -15);
+	}
 });
