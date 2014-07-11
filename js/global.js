749447cd013448ce1a4368cb863964d16c00eae7
Heidi Mason <heidi@musiclaps.com>
2014-07-10 18:06:05 -0700

    Much easier way to pull nav left

--- js/global.js
+++ js/global.js
@@ -1,16 +1,3 @@
-var navbar_right = $('.navbar-right');
-
-// Pulls navbar left in the period before Bootstrap automatically collapses it
-$(window).resize(function() {
-  if ($(window).width() <= 991) { /* Matches media query max-width on main.css */
-    navbar_right.addClass('pull-left');
-  }
-  else {
-  	navbar_right.removeClass('pull-left');
-  }
-});
-
-
 var sign_in_out = $('.sign-in-out-js');
 
 // Makes record buttons larger upon hover
