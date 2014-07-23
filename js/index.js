7b5ef19813baeb0da1bc5d2e86263356b6e58c02
Heidi Mason <heidi@musiclaps.com>
2014-07-23 09:41:02 -0700

    Organize images

--- js/index.js
+++ js/index.js
@@ -4,15 +4,15 @@ function musician_silhouette_randomizer() {
 	
 	images = new Array(5);  
 	  
-	images[0] = "<img src='img/rock-band.png' alt='Rock band silhouette'>";  
+	images[0] = "<img src='img/landing-page/rock-band.png' alt='Rock band silhouette'>";  
 	  
-	images[1] = "<img src='img/jazz-band.png' alt='Jazz band silhouette'>";  
+	images[1] = "<img src='img/landing-page/jazz-band.png' alt='Jazz band silhouette'>";  
 	  
-	images[2] = "<img src='img/bluegrass-band.png' alt='Bluegrass band silhouette'>";  
+	images[2] = "<img src='img/landing-page/bluegrass-band.png' alt='Bluegrass band silhouette'>";  
 	  
-	images[3] = "<img src='img/hip-hop-duo.png' alt='Hip-hop duo silhouette'>";  
+	images[3] = "<img src='img/landing-page/hip-hop-duo.png' alt='Hip-hop duo silhouette'>";  
 	  
-	images[4] = "<img src='img/dj-with-crowd.png' alt='Silhouette of DJ with crowd'>";  
+	images[4] = "<img src='img/landing-page/dj-with-crowd.png' alt='Silhouette of DJ with crowd'>";  
 	  
 	index = Math.floor(Math.random() * images.length);  
   
@@ -29,11 +29,11 @@ $(document).ready(function() {
 
 	$(navbar_brand_img).hover(function() 
 	{	
-		if (navbar_brand_img.attr('src') == 'img/musiclaps-logo-original-darker.png')
+		if (navbar_brand_img.attr('src') == 'img/nav-and-footer/musiclaps-logo-original-darker.png')
 		{
-			navbar_brand_img.attr('src', 'img/musiclaps-logo-original-lighter.png');
+			navbar_brand_img.attr('src', 'img/nav-and-footer/musiclaps-logo-original-lighter.png');
 		} else {
-			navbar_brand_img.attr('src', 'img/musiclaps-logo-original-darker.png');
+			navbar_brand_img.attr('src', 'img/nav-and-footer/musiclaps-logo-original-darker.png');
 		}
 	});
 
