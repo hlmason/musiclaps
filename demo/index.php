ce92032fbc650642482962d55325bf8174773ac5
Heidi Mason <heidi@musiclaps.com>
2014-07-29 11:44:17 -0700

    Polish modal design, add live player to pg

--- demo/index.php
+++ demo/index.php
@@ -7,24 +7,33 @@
 ?>
 
 	<div class="modal fade" id="selectSong" tabindex="-1" role="dialog" aria-labelledby="selectSongLabel" aria-hidden="true">
-		<div class="modal-dialog">
+		<div class="modal-dialog modal-sm">
 			<div class="modal-content">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal">
 						<span aria-hidden="true">&times;</span>
 						<span class="sr-only">Close</span>
 					</button>
-					<h4 class="modal-title" id="selectSongLabel">Select a song!</h4>
+					<h4 class="modal-title" id="selectSongLabel">Welcome to soundcheck!</h4>
 				</div>
 				<div class="modal-body">
-					Select a song from our collection to get started!
-					<!-- <p>Testing testing</p> -->
+					Would you like a practice run?<br><br>
+					<form>
+						<input type="radio" name="yes" value="yes">&nbsp; Yes<br>
+						<input type="radio" name="no" value="no">&nbsp; No<br>
+					</form>
 				</div>
 			</div>
 		</div>
 	</div>
 
 	<div id="wrapper">
+		
+		<div class="container">
+			<div class="row col-xs-12">
+				<iframe width="620" height="380" src="https://www.musiclaps.com/embed/player?songid=null" frameborder="0" scrolling="no"></iframe>
+			</div>
+		</div>
 
 	</div>
 
