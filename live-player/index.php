8686653467e8bf5ee734b21ff202ca8e1bd33857
Heidi Mason <heidi@musiclaps.com>
2014-07-31 11:37:10 -0700

    Add alt attributes

--- live-player/index.php
+++ live-player/index.php
@@ -112,15 +112,15 @@
 						<div class="col-xs-6 col-sm-5 col-sm-offset-1 col-md-4 col-md-offset-2 col-lg-3 col-lg-offset-3" id="allow-flash-instructions">
 							<h2><span>2. </span><span class="green">Allow Flash</span>.<br>(so that you can record).</h2>
 						</div>
-						<div class="col-xs-6 col-sm-5 col-md-4 col-lg-3" id="allow-flash-img" >
-							<img src="<?php echo BASE_URL; ?>img/live-player-pg/allow-flash.png">
+						<div class="col-xs-6 col-sm-5 col-md-4 col-lg-3" id="allow-flash-img">
+							<img src="<?php echo BASE_URL; ?>img/live-player-pg/allow-flash.png" alt="Pop-up">
 						</div>
 						<div class="col-sm-1 col-md-2 col-lg-3"></div>
 					</div>
 					<div class="row">
 						<div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-9 col-md-offset-2 col-lg-8 col-lg-offset-3" id="allow-flash-chrome-container">
 							<p>We recommend using <a href="https://www.google.com/intl/en_us/chrome/browser/" target="blank">Google Chrome</a>.&nbsp; In Chrome, you may also need to click "Allow" at the top of the page:</p>
-							<img src="<?php echo BASE_URL; ?>img/live-player-pg/chrome-allow-flash.png">
+							<img src="<?php echo BASE_URL; ?>img/live-player-pg/chrome-allow-flash.png" alt="Pop-up in Google Chrome">
 						</div>
 						<div class="col-sm-1 col-md-2 col-lg-3"></div>
 					</div>
@@ -135,7 +135,7 @@
 							<h2><span>3. </span>Click <span class="red">REC START</span> (above).</h2>
 						</div>
 						<div class="col-xs-6 col-sm-5 col-md-4 col-lg-3" id="recording-area-img">
-							<img src="<?php echo BASE_URL; ?>img/live-player-pg/recording-area.png">
+							<img src="<?php echo BASE_URL; ?>img/live-player-pg/recording-area.png" alt="Recording area of Live Player">
 						</div>
 						<div class="col-sm-1 col-md-2 col-lg-3"></div>
 					</div>
