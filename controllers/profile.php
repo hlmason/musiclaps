ce1b93923dc3e075235161578cd32ed0c7666b37
Heidi Mason <heidi@musiclaps.com>
2014-08-11 18:00:03 -0700

    First commit for profile pg

---
+++ controllers/profile.php
@@ -0,0 +1,13 @@
+<?php	
+
+	try {
+		$results = $db->query('SELECT firstname, picture, gender, city, state, country, talent FROM user');
+	} catch (Exception $e) {
+		echo 'Data could not be retrieved.';
+	}
+
+	// echo '<pre>';
+	// var_dump($results->fetchAll(PDO::FETCH_ASSOC));
+	$profile_results = $results->fetchAll(PDO::FETCH_ASSOC);
+
+?>
---
+++ profile/index.php
@@ -0,0 +1,43 @@
+<?php
+	require_once('../include/config.php');
+
+	$pageTitle = 'My Profile';
+	$page = 'Profile';
+	
+	include(ROOT_PATH . 'include/header.php');
+	include(ROOT_PATH . 'include/connection.php');
+	include(ROOT_PATH . 'controllers/profile.php');
+
+?>
+
+	<div id="wrapper">
+		
+		<div class="container">
+			
+			<div class="row col-xs-12">
+				
+				<div class="col-xs-1">
+					<?php 
+						echo '<div>';
+							echo $profile_result["firstname"];
+						echo '</div>';
+					?>
+				</div>
+
+				<div class="col-xs-2">
+					<?php 
+						echo '<div>';
+							echo $profile_result["gender"];
+							echo $profile_result["city"] . ',' . $profile_result["state"] . ',' . $profile_result["country"];
+						echo '</div>';
+					?>
+				</div>
+
+
+
+			</div>
+		</div>
+
+	<?php
+		include(ROOT_PATH . 'include/footer.php');
+	?>
