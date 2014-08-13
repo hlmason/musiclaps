08cb043f35693c0d467faf998e462d60b2ee87fe
Heidi Mason <heidi@musiclaps.com>
2014-08-12 18:09:05 -0700

    Add to profile pg

--- profile/index.php
+++ profile/index.php
@@ -11,31 +11,55 @@
 ?>
 
 	<div id="wrapper">
-		
-		<div class="container">
-			
-			<div class="row col-xs-12">
+		<div class="container">			
+			<div class="row col-md-10 col-md-offset-1">				
+					
+				<div class="row col-xs-12 col-sm-9">
+
+					<div class="row">	
+						<div class="col-xs-1">
+							<img id="avatar" src="<?php echo BASE_URL; ?>assets/images/profile-pg/female-avatar.png" alt="Placeholder for female Musiclaps user"><!-- Hard-coded placeholder -->
+						</div>
+						
+						<div class="col-xs-11">
+							<div class="row">
+								<div class="col-xs-10 col-sm-4">
+									<h2>$username</h2>
+								</div>
+								<div class="col-sm-4"></div>
+								<div class="col-xs-2 col-sm-4">
+									<h4>Edit Profile</h4>
+								</div>
+							</div>
+
+							<div class="row col-xs-12">
+								<h4>About me: </h4>
+							</div>
+
+							<div class="row col-xs-12">
+								<ul>
+									<li>Gender: $gender</li>
+									<li>Genre: $genre</li>
+								</ul>
+							</div>
+
+							<div class="row col-xs-12">
+								<ul>
+									<li>Location: $city, $state, $country</li>
+									<li>Talent: $talent</li>
+								</ul>
+							</div>
+						</div>
+					</div>
 				
-				<div class="col-xs-1">
-					<?php 
-						echo '<div>';
-							echo $profile_result["firstname"];
-						echo '</div>';
-					?>
+				</div>	
+				
+				<div class="row col-xs-12 col-sm-3">
+					<div id="profile-right"></div>
 				</div>
-
-				<div class="col-xs-2">
-					<?php 
-						echo '<div>';
-							echo $profile_result["gender"];
-							echo $profile_result["city"] . ',' . $profile_result["state"] . ',' . $profile_result["country"];
-						echo '</div>';
-					?>
-				</div>
-
-
-
+				
 			</div>
+			<div class="col-md-1"></div>	
 		</div>
 
 	<?php
