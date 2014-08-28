081c70e34a104a27e39cf98775f4d324cb759a6a
Heidi Mason <heidi@musiclaps.com>
2014-08-27 18:20:36 -0700

    Fix width of row borders, add to 'In Progress/Private', refactor

--- include/project-form.php
+++ include/project-form.php
@@ -22,33 +22,37 @@
 	</section>
 
 	
-	<section class="row col-xs-12" id="project-info-bar">
-		<h4 class="first-h4">$project #</h4><!-- Hard-coded placeholder -->
-		<img class="audio-mixer" id="audioMixer" src="<?php echo BASE_URL; ?>assets/images/profile-pages/audio-mixer.jpg" alt="Audio mixer icon">
-		<h4>$instrument name</h4><!-- Hard-coded placeholder -->
-		<h5>$date project started</h5>
+	<section class="row col-xs-12">
+		<div id="project-info-bar">
+			<h4 class="first-h4">$project #</h4><!-- Hard-coded placeholder -->
+			<img class="audio-mixer" id="audioMixer" src="<?php echo BASE_URL; ?>assets/images/profile-pages/audio-mixer.jpg" alt="Audio mixer icon">
+			<h4>$instrument name</h4><!-- Hard-coded placeholder -->
+			<h5>$date project started</h5>
 
-		<!-- Markup is backwards because elements are floated right -->
-		<span class="glyphicon glyphicon-trash pull-right"></span>
-		<select class="pull-right project-type">
-			<option value="private">Private</option><!-- Hard-coded placeholder -->
-		</select>
+			<!-- Markup is backwards because elements are floated right -->
+			<span class="glyphicon glyphicon-trash pull-right"></span>
+			<select class="pull-right project-type">
+				<option value="private">Private</option><!-- Hard-coded placeholder -->
+			</select>
+		</div>
 	</section>
-	<section class="row col-xs-12 volume-and-pan" id="volumeAndPan">
-		<span class="glyphicon glyphicon-volume-up"></span>
-		<div class="slider"></div>
-		<h4>100</h4><!-- Hard-coded placeholder -->
+	<section class="row col-xs-12" id="volumeAndPan">
+		<div id="volume-and-pan">
+			<span class="glyphicon glyphicon-volume-up"></span>
+			<div class="slider"></div>
+			<h4>100</h4><!-- Hard-coded placeholder -->
 
-		<h4 id="left-to-right">L</h4>
-		<div class="slider"></div>
-		<h4>R</h4>
+			<h4 id="left-to-right">L</h4>
+			<div class="slider"></div>
+			<h4>R</h4>
 
-		<span class="glyphicon glyphicon-save pull-right"></span>
-		<input class="pull-right" id="save-volume-and-pan" type="submit" value="Save">
+			<span class="glyphicon glyphicon-save pull-right"></span>
+			<input class="pull-right" id="save-volume-and-pan" type="submit" value="Save">
+		</div>
 	</section>
 
 
-	<div id="in-progress-private">
+	<form id="in-progress-private">
 		<div class="row col-xs-12">
 			<label for="title">Title
 				<input class="save pull-right" type="submit" value="Save">
@@ -86,10 +90,10 @@
 		</div>
 
 		<div class="row col-xs-12">
-			<label for="comment_display_text">Comment Display Text?
+			<label for="scrolling_comment">Scrolling Comment
 				<input class="save pull-right" type="submit" value="Save">
 			</label>
-			<textarea id="comment_display_text" name="comment_display_text"></textarea>
+			<textarea id="scrolling_comment" name="scrolling_comment"></textarea>
 		</div>
 
 		<div class="row col-xs-12">
@@ -104,7 +108,8 @@
 					</div>
 				</div>
 				<div class="col-xs-6">
-					<div id="upload-stage-background-img">
+					<div id="upload-stage-background-img"></div>
+					<div id="upload-stage-background-img-container">
 						<input class="choose-file" id="choose_stage_background_img" type="submit" value="Choose File">&nbsp; <span>No file chosen</span>
 						<div id="save-background-img-and-trash-container">
 							<input class="save-background-img" type="submit" value="Save">
@@ -114,6 +119,17 @@
 				</div>
 			</div>
 		</div>
-	</div>
+
+		<section class="row col-xs-12">
+			<h4 id="type-of-collab">Type of Collaboration</h4>
+		</section>
+		<div class="row col-xs-12">
+			<div id="type-of-collab-container">
+				<input type="submit" value="No Collab">
+				<input type="submit" value="Open Collab">
+				<input type="submit" value="Requesting Collab">
+			</div>
+		</div>
+	</form>
 
 </div>
