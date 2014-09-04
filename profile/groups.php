8d91bb5e54a91ea1edab228fd2f6295b89dd6571
Heidi Mason <heidi@musiclaps.com>
2014-09-04 13:37:54 -0700

    Polish Groups container

--- profile/groups.php
+++ profile/groups.php
@@ -23,14 +23,14 @@
 					</section>
 
 					<section class="row col-xs-12" id="groups-container">
-						<h4>Group Name: <span class="normal">$Team XY</span></h4><!-- Hard-coded placeholder -->
+						<h4 class="first-h4">Group Name: <span class="normal">$Team XY</span></h4><!-- Hard-coded placeholder -->
 						<h4>Project: 
 							<?php
 								include(ROOT_PATH . 'include/profile/group-project-listings-groups-pg.php');
 							?><!-- Hard-coded placeholder -->
 						</h4>
 						<h4>Joined: <span class="normal">1 year ago</span></h4><!-- Hard-coded placeholder -->
-						<h4>Member(s): <span class="normal">UsernameA, UsernameB, UsernameC</span></h4><!-- Hard-coded placeholder -->
+						<h4 class="last-h4">Member(s): <span class="normal">UsernameA, UsernameB, UsernameC</span></h4><!-- Hard-coded placeholder -->
 					</section>
 
 				</div>
