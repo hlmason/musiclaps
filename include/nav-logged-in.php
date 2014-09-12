f22f1dc3c74402d1cd4fe15a35fc74de0ae9959d
Heidi Mason <heidi@musiclaps.com>
2014-09-11 17:59:44 -0700

    Minor fixes for previous commit

--- include/nav-logged-in.php
+++ include/nav-logged-in.php
@@ -6,11 +6,11 @@
 					<li class="first-list-item"><a class="<?php if ($page == "My Profile") { echo "active"; } ?>" href="<?php echo BASE_URL; ?>profile/">My Profile</a></li>
 					<li><a class="<?php if ($page == "New Project") { echo "active"; } ?>" href="<?php echo BASE_URL; ?>profile/new-project.php">New Project</a></li>
 					<li><a class="<?php if ($page == "My Projects") { echo "active"; } ?>" href="<?php echo BASE_URL; ?>profile/projects.php">My Projects</a></li>
-					<li><a class="<?php if ($page == "Collaborations") { echo "active"; } ?>" href="<?php echo BASE_URL; ?>profile/collaborations.php">Collaborations</a></li>
+					<li id="middle-list-item"><a class="<?php if ($page == "Collaborations") { echo "active"; } ?>" href="<?php echo BASE_URL; ?>profile/collaborations.php">Collaborations</a></li>
 					<li><a class="<?php if ($page == "YouTube") { echo "active"; } ?>" href="#">YouTube</a></li>
 					<li><a class="<?php if ($page == "Messages") { echo "active"; } ?>" href="<?php echo BASE_URL; ?>profile/messages.php">Messages</a></li>
 					<li><a class="<?php if ($page == "Groups") { echo "active"; } ?>" href="<?php echo BASE_URL; ?>profile/groups.php">Groups</a></li>
-					<li><a class="<?php if ($page == "Favorites") { echo "active"; } ?>" href="<?php echo BASE_URL; ?>profile/favorites.php">Favorites</a></li>
+					<li id="last-list-item"><a class="<?php if ($page == "Favorites") { echo "active"; } ?>" href="<?php echo BASE_URL; ?>profile/favorites.php">Favorites</a></li>
 				</ul>
 			</nav>
 			<div class="col-lg-1"></div>
