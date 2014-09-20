515d0e7f6c98bf37a045812eff7198a268fbda95
Heidi Mason <heidi@musiclaps.com>
2014-09-19 17:59:37 -0700

    Polish responsiveness

--- include/public-project-listings.php
+++ include/public-project-listings.php
@@ -1,15 +1,15 @@
 				<a href="<?php echo BASE_URL; ?>profile/public-project.php">
 					<div class="public-project-listing">
 						<div class="row">
-							<div class="col-xs-12 col-sm-3 col-md-3 col-lg-2">
+							<div class="col-xs-12 col-sm-3 col-lg-2">
 								<img class="projectImg" src="<?php echo BASE_URL; ?>assets/images/live-player-pg/rainbow-music-notes.png" alt="Project image"><!-- Hard-coded placeholder -->
 							</div>
-							<div class="col-xs-12 col-sm-5 col-md-5 col-lg-6">
+							<div class="col-xs-12 col-sm-6 col-lg-6">
 								<h3>$Project Title</h3><!-- Hard-coded placeholder -->
 								<h4>$Username &nbsp;&nbsp;|&nbsp;&nbsp; $# plays &nbsp;&nbsp;|&nbsp;&nbsp; $Days/Months ago</h4><!-- Hard-coded placeholder -->
 								<h4>$Message from user</h4><!-- Hard-coded placeholder -->
 							</div>
-							<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+							<div class="col-xs-12 col-sm-3 col-lg-4">
 								<span class="glyphicon glyphicon-ok"></span>
 								<h4 class="last-h4">Open collab</h4>
 							</div>
@@ -20,15 +20,15 @@
 				<a href="<?php echo BASE_URL; ?>profile/public-project.php">
 					<div class="public-project-listing">
 						<div class="row">
-							<div class="col-xs-12 col-sm-3 col-md-3 col-lg-2">
+							<div class="col-xs-12 col-sm-3 col-lg-2">
 								<img class="projectImg" src="<?php echo BASE_URL; ?>assets/images/live-player-pg/rainbow-music-notes.png" alt="Project image"><!-- Hard-coded placeholder -->
 							</div>
-							<div class="col-xs-12 col-sm-5 col-md-5 col-lg-6">
+							<div class="col-xs-12 col-sm-6 col-lg-6">
 								<h3>$Project Title</h3><!-- Hard-coded placeholder -->
 								<h4>$Username &nbsp;&nbsp;|&nbsp;&nbsp; $# plays &nbsp;&nbsp;|&nbsp;&nbsp; $Days/Months ago</h4><!-- Hard-coded placeholder -->
 								<h4>$Message from user</h4><!-- Hard-coded placeholder -->
 							</div>
-							<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+							<div class="col-xs-12 col-sm-3 col-lg-4">
 								<span class="glyphicon glyphicon-ok"></span>
 								<h4 class="last-h4">Open collab</h4>
 							</div>
--- recommended/index.php
+++ recommended/index.php
@@ -12,13 +12,13 @@
 
 			<h1>Recommended</h1>
 
-			<div class="row col-xs-9">
+			<div class="row col-xs-12 col-sm-9">
 				<?php
 					include(ROOT_PATH . 'include/public-project-listings.php');
 				?>
 			</div>
 
-			<div class="row col-xs-3 ads-container">
+			<div class="row col-sm-3 ads-container"><!-- Only visible at viewport widths > 767 -->
 				<img class="first-img" src="<?php echo BASE_URL; ?>assets/images/ads/sample-ad-1.jpg" alt="Advertisement"><!-- Placeholder -->
 				<img src="<?php echo BASE_URL; ?>assets/images/ads/sample-ad-2.jpg" alt="Advertisement"><!-- Placeholder -->
 				<img src="<?php echo BASE_URL; ?>assets/images/ads/sample-ad-3.jpg" alt="Advertisement"><!-- Placeholder -->
