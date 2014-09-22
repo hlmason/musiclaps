af7b086bd1dee31c0aefe3b2482bd9f35b30363b
Heidi Mason <heidi@musiclaps.com>
2014-09-22 14:20:50 -0700

    Refactor link colors, add comments

--- assets/javascripts/profile/nav.js
+++ assets/javascripts/profile/nav.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	var ul_navLoggedIn = $('ul.navLoggedIn');
 
-	$('#glyphiconUser').on('click', function() {
+	$('#glyphiconUser').on('click', function() { // This does the same thing as the function below
 		if ( ul_navLoggedIn.is(':hidden') ) {
 			ul_navLoggedIn.slideDown('slow');
 		}
@@ -10,8 +10,8 @@ $(document).ready(function() {
 			ul_navLoggedIn.slideUp('slow');
 		}
 	});
-
-	$('h4.navLoggedIn').on('click', function() {
+	
+	$('h4.navLoggedIn').on('click', function() { // This does the same thing as the function above
 		if ( ul_navLoggedIn.is(':hidden') ) {
 			ul_navLoggedIn.slideDown('slow');
 		}
