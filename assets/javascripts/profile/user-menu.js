cc0f0bd325984015c4985718e913e78747be85bc
Heidi Mason <heidi@musiclaps.com>
2014-09-24 15:27:57 -0700

    Minor fix for User Menu in uncollapsed mode

--- assets/javascripts/profile/user-menu.js
+++ assets/javascripts/profile/user-menu.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	var userMenu = $('ul.userMenu');
 
-	$('.glyphiconUser').on('click', function() { // This does the same thing as the function below
+	$('.glyphiconUser').on('click', function() { // Does the same thing as function below
 		if ( userMenu.is(':hidden') ) {
 			userMenu.slideDown('fast');
 		}
@@ -11,13 +11,19 @@ $(document).ready(function() {
 		}
 	});
 	
-	$('h4.userMenu').on('click', function() { // This does the same thing as the function above
+	$('h4.userMenu').on('click', function() { // Does the same thing as function above
 		if ( userMenu.is(':hidden') ) {
 			userMenu.slideDown('fast');
 		}
 		else if ( userMenu.is(':visible') ) {
 			userMenu.slideUp('fast');
 		}
+	});
+
+	$(window).resize(function() { // Makes menu appear when window is made larger than 991px after ul.menu has been slidden up 
+	  	if ($(window).width() > 991) {
+	     	userMenu.show();
+	  	}
 	});
 
 });
