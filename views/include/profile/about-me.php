d5df26be11a8f7b6adae3040fdddca3ac754e444
Heidi Mason <heidi@musiclaps.com>
2014-10-14 17:24:24 -0700

    Refactor About Me div

---
+++ views/include/profile/about-me.php
@@ -0,0 +1,14 @@
+					<div class="row">
+						<div class="col-sm-3"></div>
+						<div class="col-xs-12 col-sm-8 about-me">
+							<h4>About me:</h4>
+							<ul class="first-ul-profile-page">
+								<li class="profile-page"><span class="glyphicon glyphicon-user"></span>&nbsp; $gender</li><!-- Hard-coded placeholder -->
+								<li class="profile-page glyphicon-home"><span class="glyphicon glyphicon-home"></span>&nbsp; $website</li><!-- Hard-coded placeholder -->
+							</ul>
+							<ul class="second-ul-profile-page">
+								<li class="profile-page"><span class="glyphicon glyphicon-globe"></span>&nbsp; $city, $state, $country</li><!-- Hard-coded placeholder -->
+							</ul>
+						</div>
+						<div class="col-sm-1"></div>
+					</div>
--- views/profile/index.php
+++ views/profile/index.php
@@ -36,20 +36,12 @@
 						</div>
 					</div>
 						
-					<div class="row">
-						<div class="col-sm-3"></div>
-						<div class="col-xs-12 col-sm-8" id="about-me">
-							<h4>About me:</h4>
-							<ul class="first-ul-profile-page">
-								<li class="profile-page"><span class="glyphicon glyphicon-user"></span>&nbsp; $gender</li><!-- Hard-coded placeholder -->
-								<li class="profile-page" id="glyphicon-home"><span class="glyphicon glyphicon-home"></span>&nbsp; $website</li><!-- Hard-coded placeholder -->
-							</ul>
-							<ul class="second-ul-profile-page">
-								<li class="profile-page"><span class="glyphicon glyphicon-globe"></span>&nbsp; $city, $state, $country</li><!-- Hard-coded placeholder -->
-							</ul>
-						</div>
-						<div class="col-sm-1"></div>
-					</div>
+					<!-- "About me" div -->
+					<?php
+						include(ROOT_PATH . 'views/include/profile/about-me.php');
+					?>
+					<!-- End of "About me" div -->
+
 
 					<!-- "My Public Projects" -->
 					<section id="myPublicProjects">
