b9dac182320ad9fcc1dc12bf73ae2c541b963754
Heidi Mason <heidi@musiclaps.com>
2014-12-16 13:11:05 -0800

    Add hover to Public Profile project tiles per Go

--- views/include/public-project/public-project-listings-b-logged-in.php
+++ views/include/public-project/public-project-listings-b-logged-in.php
@@ -1,19 +1,23 @@
-								<section class="col-xs-12 col-md-6 public-projects-and-collaborations">
-									<div class="row col-xs-6 col-sm-4 col-md-6">
-										<a href="<?php echo BASE_URL; ?>views/public-project/project-details-logged-in.php"><img class="projectImg" src="<?php echo BASE_URL; ?>assets/images/live-player-pg/rainbow-music-notes.png" alt="Project image"></a><!-- Hard-coded placeholder -->
-									</div>
-									<div class="row col-xs-6 col-sm-8 col-md-6">
-										<a href="<?php echo BASE_URL; ?>views/public-project/project-details-logged-in.php"><h4>$Project Title</h4></a><!-- Hard-coded placeholder -->
-										<h5>$#### plays</h5><!-- Hard-coded placeholder -->
-									</div>
-								</section>
+								<a href="<?php echo BASE_URL; ?>views/public-project/project-details-logged-in.php">
+									<section class="col-xs-12 col-md-6 public-projects-and-collaborations">
+										<div class="row col-xs-6 col-sm-4 col-md-6">
+											<img class="projectImg" src="<?php echo BASE_URL; ?>assets/images/live-player-pg/rainbow-music-notes.png" alt="Project image"><!-- Hard-coded placeholder -->
+										</div>
+										<div class="row col-xs-6 col-sm-8 col-md-6">
+											<h4>$Project Title</h4><!-- Hard-coded placeholder -->
+											<h5>$#### plays</h5><!-- Hard-coded placeholder -->
+										</div>
+									</section>
+								</a>
 
-								<section class="col-xs-12 col-md-6 public-projects-and-collaborations">
-									<div class="row col-xs-6 col-sm-4 col-md-6">
-										<a href="<?php echo BASE_URL; ?>views/public-project/project-details-logged-in.php"><img class="projectImg" src="<?php echo BASE_URL; ?>assets/images/live-player-pg/rainbow-music-notes.png" alt="Project image"></a><!-- Hard-coded placeholder -->
-									</div>
-									<div class="row col-xs-6 col-sm-8 col-md-6">
-										<a href="<?php echo BASE_URL; ?>views/public-project/project-details-logged-in.php"><h4>$Project Title</h4></a><!-- Hard-coded placeholder -->
-										<h5>$#### plays</h5><!-- Hard-coded placeholder -->
-									</div>
-								</section>
+								<a href="<?php echo BASE_URL; ?>views/public-project/project-details-logged-in.php">
+									<section class="col-xs-12 col-md-6 public-projects-and-collaborations">
+										<div class="row col-xs-6 col-sm-4 col-md-6">
+											<img class="projectImg" src="<?php echo BASE_URL; ?>assets/images/live-player-pg/rainbow-music-notes.png" alt="Project image"><!-- Hard-coded placeholder -->
+										</div>
+										<div class="row col-xs-6 col-sm-8 col-md-6">
+											<h4>$Project Title</h4><!-- Hard-coded placeholder -->
+											<h5>$#### plays</h5><!-- Hard-coded placeholder -->
+										</div>
+									</section>
+								</a>
