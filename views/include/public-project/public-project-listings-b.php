e5c853cebe75f664ec5fa501aaa331a628c1acfa
Heidi Mason <heidi@musiclaps.com>
2014-12-18 17:36:13 -0800

    Make '# of plays' h5 gray, add tiles to Public Profile pgs

--- views/include/public-project/public-project-listings-b.php
+++ views/include/public-project/public-project-listings-b.php
@@ -4,6 +4,18 @@
 											<img class="projectImg" src="<?php echo BASE_URL; ?>assets/images/live-player-pg/rainbow-music-notes.png" alt="Project image"><!-- Hard-coded placeholder -->
 										</div>
 										<div class="row col-xs-6 col-sm-8 col-md-6">
+											<h4>$Super Long Project Title</h4><!-- Hard-coded placeholder -->
+											<h5>$#### plays</h5><!-- Hard-coded placeholder -->
+										</div>
+									</section>
+								</a>
+
+								<a href="<?php echo BASE_URL; ?>views/public-project/project-details.php">
+									<section class="col-xs-12 col-md-6 public-projects-and-collaborations">
+										<div class="row col-xs-6 col-sm-4 col-md-6">
+											<img class="projectImg" src="<?php echo BASE_URL; ?>assets/images/live-player-pg/rainbow-music-notes.png" alt="Project image"><!-- Hard-coded placeholder -->
+										</div>
+										<div class="row col-xs-6 col-sm-8 col-md-6">
 											<h4>$Project Title</h4><!-- Hard-coded placeholder -->
 											<h5>$#### plays</h5><!-- Hard-coded placeholder -->
 										</div>
@@ -17,6 +29,18 @@
 										</div>
 										<div class="row col-xs-6 col-sm-8 col-md-6">
 											<h4>$Project Title</h4><!-- Hard-coded placeholder -->
+											<h5>$#### plays</h5><!-- Hard-coded placeholder -->
+										</div>
+									</section>
+								</a>
+
+								<a href="<?php echo BASE_URL; ?>views/public-project/project-details.php">
+									<section class="col-xs-12 col-md-6 public-projects-and-collaborations">
+										<div class="row col-xs-6 col-sm-4 col-md-6">
+											<img class="projectImg" src="<?php echo BASE_URL; ?>assets/images/live-player-pg/rainbow-music-notes.png" alt="Project image"><!-- Hard-coded placeholder -->
+										</div>
+										<div class="row col-xs-6 col-sm-8 col-md-6">
+											<h4>$Long Project Title</h4><!-- Hard-coded placeholder -->
 											<h5>$#### plays</h5><!-- Hard-coded placeholder -->
 										</div>
 									</section>
