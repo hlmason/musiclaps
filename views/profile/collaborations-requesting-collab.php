063cabbe9840310bd072084d914bcf60f0d51240
Heidi Mason <heidi@musiclaps.com>
2014-12-18 17:44:17 -0800

    Add additional tiles to check CSS

--- views/profile/collaborations-requesting-collab.php
+++ views/profile/collaborations-requesting-collab.php
@@ -31,6 +31,24 @@
 						</div>
 					</section>
 
+					<section class="row projects-container" id="requestingCollabContainer">
+						<div class="col-xs-5 col-sm-3 col-md-3">
+							<span class="glyphicon-pencil-container"><span class="glyphicon glyphicon-pencil glyphiconPencil"></span></span>
+							<img class="projectImg" src="<?php echo BASE_URL; ?>assets/images/live-player-pg/rainbow-music-notes.png" alt="Project image"><!-- Hard-coded placeholder -->
+						</div>
+					
+						<div class="col-xs-7 col-sm-6 col-md-7">
+							<h4>$project title</h4><!-- Hard-coded placeholder -->
+							<h5>Upload Date: <span class="normal">$# days ago</span></h5><!-- Hard-coded placeholder -->
+							<h5>Plays: <span class="normal">$#</span></h5><!-- Hard-coded placeholder -->
+							<h5 class="last-h5 xs">Requesting Collab <span class="glyphicon glyphicon-lock"></span></h5><!-- Only visible at viewport widths <= 767px -->
+						</div>
+
+						<div class="col-sm-3 col-md-2">
+							<h5 class="last-h5 sm">Requesting Collab <span class="glyphicon glyphicon-lock"></span></h5><!-- Only visible at viewport widths > 767px -->
+						</div>
+					</section>
+
 					<div id="requestingCollabForm">
 						<!-- Recording Player -->
 						<div class="row col-xs-12 live-player-container">
