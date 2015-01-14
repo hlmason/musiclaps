689d00b40126caa3db8526bfa9acdd356f2b5211
Heidi Mason <heidi@musiclaps.com>
2015-01-14 14:19:02 -0800

    Add HTML slider inputs

--- views/include/profile/volume-and-pan.php
+++ views/include/profile/volume-and-pan.php
@@ -2,11 +2,11 @@
 		<form class="project-form" action="" method="post">
 			<div class="volume-and-pan sm"><!-- Only visible at viewport widths > 580px -->
 				<span class="glyphicon glyphicon-volume-up"></span>
-				<div class="slider"></div>
+				<input class="slider" name="slider" type="range" min=0 max=100 value=0>
 				<h4>100</h4><!-- Hard-coded placeholder -->
 
 				<h4 class="left-to-right">L</h4>
-				<div class="slider"></div>
+				<input class="slider" name="slider" type="range" min=0 max=100 value=0>
 				<h4>R</h4>
 
 				<span class="glyphicon glyphicon-save"></span>
@@ -16,13 +16,13 @@
 			<div class="volume-and-pan xs"><!-- Only visible at viewport widths <= 580px -->
 				<div>
 					<span class="glyphicon glyphicon-volume-up"></span>
-					<div class="slider"></div>
+					<input class="slider" name="slider" type="range" min=0 max=100 value=0>
 					<h4>100</h4><!-- Hard-coded placeholder -->
 				</div>
 
 				<div>
 					<h4 class="left-to-right">L</h4>
-					<div class="slider"></div>
+					<input class="slider" name="slider" type="range" min=0 max=100 value=0>
 					<h4 class="right">R</h4>
 				</div>
 
